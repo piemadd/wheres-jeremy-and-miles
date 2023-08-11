cc1ce973cc5599fbb1001fe25f531f86fd42e582
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-08-11 05:12:01 -0500

    finally fixed schedule

--- src/Schedule.jsx
+++ src/Schedule.jsx
@@ -2,20 +2,20 @@ const StopsSchedule = ({ stops }) => {
   return (
     <section className='busStops'>
       {stops.map((stop, i) => {
-        //console.log(stop);
+        console.log(stop);
 
         const arr = new Date(
-          stop.estimated_arrival_time_unix
-            ? stop.estimated_arrival_time_unix * 1000
+          stop.live_arrival_time_unix
+            ? stop.live_arrival_time_unix * 1000
             : stop.scheduled_arrival_time_unix * 1000
         );
-        const arrSch = stop.estimated_arrival_time_unix ? false : true;
+        const arrSch = stop.live_arrival_time_unix === null ? true : false;
         const dep = new Date(
-          stop.estimated_departure_time_unix
-            ? stop.estimated_departure_time_unix * 1000
+          stop.live_departure_time_unix
+            ? stop.live_departure_time_unix * 1000
             : stop.scheduled_departure_time_unix * 1000
         );
-        const depSch = stop.estimated_departure_time_unix ? false : true;
+        const depSch = stop.live_departure_time_unix === null ? true : false;
 
         const dateAndTimeFormatter = new Intl.DateTimeFormat("en-US", {
           month: "short",
@@ -26,12 +26,16 @@ const StopsSchedule = ({ stops }) => {
           timeZoneName: "short",
         });
 
-        console.log(`hsl(${Math.floor((i / stops.length) * 360)}, 68%, 40%)`)
-
         return (
-          <div key={i} className="busStop" style={{
-            backgroundColor: `hsl(${Math.floor((i / stops.length) * 360)}, 68%, 40%)`
-          }}>
+          <div
+            key={i}
+            className='busStop'
+            style={{
+              backgroundColor: `hsl(${Math.floor(
+                (i / stops.length) * 360
+              )}, 68%, 40%)`,
+            }}
+          >
             <h3>{stop.name.split(":")[0]}</h3>
             <p>
               <b>
